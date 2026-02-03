922121506eb873208b43371c2a235be66cb44c80
Hoang Nghia Hoa <hoabinh123thpt@gmail.com>
2026-02-03 10:00:37 +0700

    Hoàn thiện cấu trúc Monorepo: Backend và Frontend

---
+++ Backend/src/test/java/com/urlshortener/quan_ly_tai_san_backend/QuanLyTaiSanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urlshortener.quan_ly_tai_san_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanLyTaiSanBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
